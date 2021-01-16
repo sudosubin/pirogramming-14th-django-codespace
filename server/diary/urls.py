b275f3699e8d8dad17f8114f12faa04e99641891
sudosubin <sudosubin+github@gmail.com>
2021-01-16 07:19:05 +0000

    Add article create view

--- server/diary/urls.py
+++ server/diary/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from diary.views import article_list, article_read
+from diary.views import article_list, article_read, article_create
 
 
 app_name = 'diary'
@@ -9,4 +9,5 @@ app_name = 'diary'
 urlpatterns = [
     path('', article_list, name='article_list'),
     path('article/<int:pk>/', article_read, name='article_read'),
+    path('article/create/', article_create, name='article_create'),
 ]
--- server/diary/views.py
+++ server/diary/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
 
 from diary.models import Article
 
 # MTV
 # Model, Template, View(python code)
+
+# CRUD
+# R + L
+# CUD
 
 
 def article_list(request):
@@ -20,3 +26,17 @@ def article_read(request, pk):
         'article': article,
     }
     return render(request, 'diary/read.html', context)
+
+
+@csrf_exempt
+def article_create(request):
+    if request.method == 'GET':
+        return render(request, 'diary/create.html')
+
+    title = request.POST['title']
+    content = request.POST['content']
+
+    article = Article.objects.create(title=title, content=content)
+
+    url = reverse('diary:article_read', kwargs={'pk': article.id})
+    return redirect(url)
