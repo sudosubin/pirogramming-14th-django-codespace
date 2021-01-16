315ea4bde85ed1083b6c2c7862cb94351bd99058
sudosubin <sudosubin+github@gmail.com>
2021-01-16 06:07:23 +0000

    Add diary article list

--- server/diary/views.py
+++ server/diary/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from diary.models import Article
+
+# MTV
+# Model, Template, View(python code)
+
 
 def article_list(request):
-    return render(request, 'diary/list.html', {})
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'diary/list.html', context)
